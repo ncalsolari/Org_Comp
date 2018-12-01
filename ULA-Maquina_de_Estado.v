2f05f7303e4db2d4c4c4671f3a3cc0ed592f9975
SrNetoo <ncalsolari@usp.br>
2018-12-01 14:13:26 -0200

    Update ULA-Maquina_de_Estado.v

--- ULA-Maquina_de_Estado.v
+++ ULA-Maquina_de_Estado.v
@@ -1,12 +1,12 @@
 module ULA(
-           input [3:0] X1,X2,  // ALU 8-bit Inputs                 
-           input [2:0] CNTRL,// ALU Selection
-           output [7:0] Y, // ALU 8-bit Output
-           output CarryOut // Carry Out Flag
+	input [3:0] X1,X2,  // ULA 8-bit de entrada, 2 numeros                
+	input [2:0] CNTRL,// Contorle da ULA
+	output [7:0] Y, // ULA 8-bit Output
+           output CarryOut // Carry Out
     );
     reg [7:0] ALU_Result;
     wire [4:0] tmp;
-    assign Y = ALU_Result; // ALU out
+    assign Y = ALU_Result; // Saida da ULA
     assign tmp = {4'b0000,X1} + {4'b0000,X2};
     assign CarryOut = tmp[4]; // Carryout flag
     always @(*)
@@ -31,30 +31,30 @@ endmodule
 
 module maquina_de_estados( reset, comando);
 	
-	input [4:0] comando;
-	reg [4:0] estadoAtual;
+	input [2:0] comando;
+	reg [2:0] estadoAtual;
 	reg [2:0] saida;
   
-	//acho que falta mudar esse 4' pra 5'
-	parameter S2 = 4'd0, S1 = 4'd1, S8 = 4'd2, S3 = 4'd3,
-	S7 = 4'd4, S0 = 4'd5, S4 = 4'd6, S3I = 4'd7, S6 = 4'd8,
-	blank = 4'd9;
+	//declarando os estados
+	parameter S2 = 3'd0, S1 = 3'd1, S8 = 3'd2, S3 = 3'd3,
+	S7 = 3'd4, S0 = 3'd5, S4 = 3'd6, S3I = 3'd7, S6 = 3'd8,
+	blank = 3'd9;
 	
-	always @ (*)
+	always @ (*)//organizando e selecionando o funciomnametno da troca de estados
 		begin
 			
 			else
 				case (estadoAtual)
 					S0:
-						if (comando == 5'b00000)
+						if (comando == 3'b000)
 							estadoAtual <= S1;
-						else if (comando == 5'b00001)
+					else if (comando == 3'b001)
 							estadoAtual <= S2;
-						else if (comando == 5'b00010)
+					else if (comando == 3'b010)
 							estadoAtual <= S3;
-						else if (comando == 5'b00011)
+					else if (comando == 3'b011)
 							estadoAtual <= S4;
-						else if (comando == 5'b00100)
+					else if (comando == 3'b100)
 							estadoAtual <= S5
 							
 					S1:
@@ -76,7 +76,7 @@ module maquina_de_estados( reset, comando);
 				endcase
 		end
 		
-		always @ (estadoAtual)
+	always @ (estadoAtual) // saida da maquina de estados mediante seu estado atual
 		begin
 			case (estadoAtual)
 				S1:
@@ -92,9 +92,54 @@ module maquina_de_estados( reset, comando);
 				
 			endcase
 		end	
-	//n sei pra que serve isso ainda	
-	display_verilog display1(.W(saida[3]), .X(saida[2]), .Y(saida[1]), .Z(saida[0]), .saida(display));
-	temporizador t1(.clockin(clock_placa), .clockout(clock_dividido));
 	
+	
+endmodule
+
+
+module memo_inst
+
+	(clock, op_counter, inst_out);
+
+	input [15:0] op_counter;
+	output [15:0] inst_out;
+
+	reg [15:0] memoria_instrucoes [99:0]; // vetor para armazenar os bits
+	always @(posedge clock)
+
+		assign inst_out = memoria_instrucoes[op_counter]; //recebe o OP counter e faz apassagem de 16 bits para a saida
+
+endmodule
+
+
+module banco_registradores
+
+	(clock, reg1, reg2, reg3, inp, reg1valor, reg2valor, reg3valor);
+
+input clock;
+
+	input [1:0] reg1; // endereço dos registradores
+	input [1:0] reg2;
+	input [1:0] reg3;
+
+	input [15:0] in_ula;// valor que vem da ula, armazenada no registrador 3
+
+	reg [15:0] registradores[15:0];
+	
+	output [15:0] reg1vaor; // valores armazenados nos registradores
+	output [15:0] reg2valor;
+	output [15:0] reg3valor;
+
+	
+
+always @(posedge clock)
+begin
+	registradores[reg3] <= in_ula; 
+end
+
+	assign reg1valor = registradores[reg1];
+	assign reg2valor = registradores[reg2];
+	assign reg3valor = registradores[reg3];
+
 endmodule
 	
